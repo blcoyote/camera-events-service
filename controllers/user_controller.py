ac96e7f5a97b3c9352528e3a792f19f6d93a9f83
blcoyote <30732216+blcoyote@users.noreply.github.com>
2023-06-27 13:24:53 +0200

    add user controller

---
+++ controllers/user_controller.py
@@ -0,0 +1,34 @@
+from typing import Annotated, List
+from fastapi import APIRouter, Depends, HTTPException
+from loguru import logger
+from lib.websockets import get_connection_manager
+from models.user import User, UserCreate, UserGet
+from database import schema, crud, database
+from lib.auth import get_current_active_user
+from sqlalchemy.orm import Session
+
+manager = get_connection_manager()
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    dependencies=[Depends(get_current_active_user)],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/users/me", response_model=UserGet)
+async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
+
+@router.get("/users/", response_model=list[User])
+def read_users( skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@router.post("/users/", response_model=UserGet)
+def create_user(user: UserCreate, current_user: Annotated[User, Depends(get_current_active_user)], db: Session = Depends(database.get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
-from controllers import event_controller
-from lib.auth import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token, get_current_active_user
+from controllers import event_controller, user_controller
+from lib.auth import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token
 from lib.settings import get_settings
 from loguru import logger
 from lib.websockets import get_connection_manager
 from sqlalchemy.orm import Session
 from models.token import Token
-from models.user import User, UserCreate, UserGet
-from database import schema, crud, database
+from database import schema, database
 
 schema.Base.metadata.create_all(bind=database.engine)
 
@@ -21,6 +20,7 @@ logger.add(f"./log/apilog_{datetime.now().strftime('%Y-%m-%d')}.log", rotation="
 
 app = FastAPI(title=get_settings().app_name, version=get_settings().app_version, debug=False)
 app.include_router(event_controller.router)
+app.include_router(user_controller.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -31,10 +31,6 @@ app.add_middleware(
 
 
 manager = get_connection_manager()
-
-@app.get("/" ) 
-def read_root():
-    return {"Hello": "World"}
 
 @app.get("/application-configuration")
 def app_config():
@@ -58,18 +54,3 @@ async def login_for_access_token(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-@app.get("/users/me", response_model=UserGet)
-async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
-    return current_user
-
-@app.get("/users/", response_model=list[User])
-def read_users( current_user: Annotated[User, Depends(get_current_active_user)], skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-@app.post("/users/", response_model=UserGet)
-def create_user(user: UserCreate, current_user: Annotated[User, Depends(get_current_active_user)], db: Session = Depends(database.get_db) ):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
