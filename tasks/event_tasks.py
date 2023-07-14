f1648d1dd61c6d5e62c1644a3d44453e6a43e478
blcoyote <30732216+blcoyote@users.noreply.github.com>
2023-07-14 08:14:33 +0200

    update

--- tasks/event_tasks.py
+++ tasks/event_tasks.py
@@ -40,6 +40,12 @@ def get_snapshot(id: str) -> bytes:
     response = requests.get(url).content
     return response
 
+def get_latest(camera: str) -> bytes:
+    url = f"{get_settings().frigate_baseurl}/api/{camera}/latest.jpg"
+    response = requests.get(url).content
+    return response
+
+
 def get_clip(id: str) -> bytes:
     url = f"{get_settings().frigate_baseurl}/api/events/{id}/clip.mp4"
     response = requests.get(url).content
