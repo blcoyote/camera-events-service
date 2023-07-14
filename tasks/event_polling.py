9d163f8c867ebec70bfbbd9835d6f75397041464
blcoyote <30732216+blcoyote@users.noreply.github.com>
2023-07-14 10:16:29 +0200

    Update event_polling.py

--- tasks/event_polling.py
+++ tasks/event_polling.py
@@ -11,25 +11,25 @@ POLLING_INTERVAL = 30
 manager = get_connection_manager()
 
 async def poll_for_new_events():
-    
+    aftertime = datetime.now()- timedelta(seconds = POLLING_INTERVAL)
     while True:
-        #logger.info("Polling for new events...")
+        logger.info(f"Polling for new events from {aftertime}")
         params = CameraEventQueryParams()
-        aftertime = datetime.now()- timedelta(seconds = POLLING_INTERVAL)
         params.after=int(aftertime.timestamp())
-        params.limit=2000
+        params.limit=200
+
+        aftertime = datetime.now() #next run
 
         events = get_events(params)
+        
 
         if len(events) > 0:
             logger.info(f"Found {len(events)} events. Pushing to clients")
             for event in events:
-
-
                 try:
                     #await manager.broadcast(wsEvent.json())
                     send_topic_push(event)
                 except Exception as e:
                     logger.error(f"Error sending event to firebase: {e}")
-
+        
         await asyncio.sleep(POLLING_INTERVAL)
